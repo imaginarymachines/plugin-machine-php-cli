9890f3163168192b95c8e02b2b1071f8d8bbe01a
Josh Pollock <jpollock412@gmail.com>
2021-12-03 13:33:33 -0500

    v0.4.1

--- app/Commands/Version.php
+++ app/Commands/Version.php
@@ -28,7 +28,7 @@ class Version extends Command
 	 */
 	public function handle()
 	{
-		$this->info(sprintf('Version is: %s', '0.4.0'));
+		$this->info(sprintf('Version is: %s', '0.4.1'));
 	}
 
 	/**
