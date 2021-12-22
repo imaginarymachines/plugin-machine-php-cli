ce40258f2ab7a049ddb7c98b7508930ea0a16978
Josh Pollock <jpollock412@gmail.com>
2021-12-22 10:59:51 -0500

    Add debug comamnd

---
+++ app/Commands/Debug.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+use App\Helpers;
+use App\Services\Features;
+
+class Debug extends Command
+{
+
+    protected $signature = 'debug';
+
+
+    protected $description = 'Print debub info';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle(Features $features)
+    {
+        $this->info('App path: ' . app_path());
+        $this->info('Write path: ' . Helpers::writePath());
+        $this->info('Rules path: ' . app_path(Features::PATH_RULES));
+        $this->info('Rules exist: ' . $features->rulesDataExists());
+        $this->info('Features path: ' . $features->featuresDataExists());
+        $this->info('Features exist: ' . $features->featuresDataExists());
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
